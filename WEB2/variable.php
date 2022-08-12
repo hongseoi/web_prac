6b8286ce9ae447e4bdddbdd60e9008dba6d3dba1
seoi hong <90823683+hongseoi@users.noreply.github.com>
2022-08-13 00:26:46 +0900

    add

--- WEB2/variable.php
+++ WEB2/variable.php
@@ -4,7 +4,31 @@
         <h1>Variables</h1>
 
         <?php
-        echo "Lorem ipsum is placeholder text commonly used in the graphic, print, and publishing industries for previewing layouts and visual mockups.";
+        $name = "egoing";
+        echo "Lorem ipsum is placeholder text ".$name.
+        "commonly used in the graphic, 
+        print, and publishing industries 
+        for previewing layouts and visual mockups. by ".$name;
+        
+        class foo{
+            var $bar = 'I am bar';
+            var $arr = array('I am A.','I am B.', 'I am C');
+            var $r = 'I am r.';
+        }
+
+        $foo = new foo();
+        $bar = 'bar';
+        $baz = array('foo','bar','baz','quux');
+        ehco $foo -> $bar."\n";
+        ech $foo -> {$baz[1]}."\n";
+
+        $start = 'b';
+        $end = 'ar';
+        echo $foo -> {$start.$end}."\n";
+
+        $arr = 'arr';
+        echo $foo -> {$arr[1]}."\n";
         ?>
+
     </body>
 </html>
